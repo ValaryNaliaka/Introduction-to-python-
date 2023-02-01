735423b8aaacc9350ac61c2d6b7feed2520af7d0
ValaryNaliaka <122798524+ValaryNaliaka@users.noreply.github.com>
2023-02-01 06:38:38 +0200

    Required people to vote

---
+++ LogicalOperator.py
@@ -0,0 +1,8 @@
+a=("nationality")
+a=['Kenya','Uganda','Tanzania']
+age=int(input("enter your age"))
+nationality=input("enter your nationality")
+if(age>=18) and (nationality=="a"):
+    print("not  eligible to vote")
+else:
+    print("  eligible to vote")
